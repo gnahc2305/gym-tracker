a8b044c941531573dbae7fd58a83d8ccef1f4d84
gnahc2305 <achangmurillo@gmail.com>
2023-08-01 16:18:46 -0500

    create home page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function dashboard() {
+  return (
+    <div>
+        <h1>Gym Tracker</h1>
+        <h3>Insert details of workout, and record progress</h3>
+    </div>
+  )
+}
+
+export default dashboard
