2348413ef692fc65343016f17ebb49b0270517b0
gnahc2305 <achangmurillo@gmail.com>
2023-08-01 16:44:54 -0500

    loggin in redirects user to the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+// import { redirect } from "next/dist/server/api-utils";
+import redirect from 'next/navigation'
+import { Redirect } from "next";
+import { SignInButton } from "@/Components/buttons";
 
 export default function Home() {
+
+  async function checkIfAuthenticated() {
+    const session = await getServerSession();
+  
+    console.log(session);
+
+    // if (session) {
+    //   Redirect('/dashboard');
+    //   redirect('/dashboard');
+    // }
+  }
+
+  checkIfAuthenticated();
+
+
+
   return (
     <main>
       <div className="flex flex-col justify-between p-14 text-[50px]">
@@ -9,9 +30,10 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center text-[30px] pt-24">
-        <Link href='/api/auth/signin'>
+        <SignInButton />
+        {/* <Link href='/api/auth/signin'>
           <button className="">Sign In</button>
-        </Link>
+        </Link> */}
       </div>
     </main>
   );
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
