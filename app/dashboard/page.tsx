87ebf404de2cc1b1cee7a506fe21c0ed6084f92d
gnahc2305 <achangmurillo@gmail.com>
2023-08-02 15:42:34 -0500

    start layout of form

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,13 +16,27 @@ function dashboard() {
               <SignInButton />
             </div>
             <AuthCheck>
-              {/* {redirect('/')} */}
               <SignOutButton />
             </AuthCheck>
           </div>
         </div>
       </nav>
-      <h3>Insert details of workout, and record progress</h3>
+
+      <div className="flex p-10">
+        <div className="bg-gray-300">
+          <h3>Insert details of workout, and record progress</h3>
+
+          <div className="mt-10 h-[400px] w-full bg-slate-800">
+            <form action="">
+              <input type="date" id="date" name="date" required />
+            </form>
+          </div>
+        </div>
+
+        <div className="ml-14">
+          Tables
+        </div>
+      </div>
     </div>
   );
 }
