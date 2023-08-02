4a4e7345e20fde3312988e89a1188ba100b9a150
gnahc2305 <achangmurillo@gmail.com>
2023-08-02 15:24:44 -0500

    setup prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth/next";
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
