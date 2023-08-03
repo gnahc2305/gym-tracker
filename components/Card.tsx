da721e0c46b4844a838cd2a5033dd1fb54359d76
gnahc2305 <achangmurillo@gmail.com>
2023-08-02 19:30:48 -0500

    create card layout

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Card() {
+  let demo: string = 'Machine Chest Press/100Kg/10/14'
+  let date: string = '23/23/2004'
+
   return (
-    <div className='h-[300px] w-[300px] bg-slate-300'>
-        s
+    <div className='h-[300px] w-[300px] bg-slate-300 p-4'>
+        <h2 className='mb-3'>{date}</h2>
+        <h1 className='mb-2'>{demo}</h1>
+        <h1 className='mb-2'>{demo}</h1>
+        <h1 className='mb-2'>{demo}</h1>
+        <h1 className='mb-2'>{demo}</h1>
     </div>
   )
 }
