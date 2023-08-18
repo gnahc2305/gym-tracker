9e2476aa03a63f20fe5f40f6c8877709f794e2ea
gnahc2305 <achangmurillo@gmail.com>
2023-08-18 14:57:06 -0500

    change Link to a tag to improve data fetching

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,7 +14,8 @@ function Card({
   },
 }: any) {
   return (
-    <Link href={`/dashboard/${id}`} prefetch={false}>
+    // <Link href={`/dashboard/${id}`} prefetch={false}>
+    <a href={`/dashboard/${id}`}>
       <div className=" bg-white py-2 px-5 text-lg rounded-md h-full hover:bg-blue-200">
         <h2 className="mb-3 text-[20px]">{date}</h2>
         <h1 className="mb-2">{exercise1}</h1>
@@ -24,7 +25,8 @@ function Card({
         <h1 className="mb-2">{exercise5}</h1>
         <h1 className="mb-2">{exercise6}</h1>
       </div>
-    </Link>
+    </a>
+    // </Link>
   );
 }
 
